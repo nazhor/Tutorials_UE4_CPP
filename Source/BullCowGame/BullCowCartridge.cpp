967fd0979e7422004c68c2021061f6a34e12d3e7
David <dbernad4220@gmail.com>
2020-02-03 19:25:33 +0100

    First steps ue4 c++

---
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine(TEXT("Hi there! MOOO"));
+    PrintLine(TEXT("Time to play"));
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    ClearScreen();
+    FString HiddenWord = TEXT("Arduino");
+}
