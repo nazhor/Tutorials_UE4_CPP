032bdf0ab2e33688e0cf389829d61bae58cfbd89
David <dbernad4220@gmail.com>
2020-02-11 19:17:22 +0100

    String formatting

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -6,10 +6,43 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     Super::BeginPlay();
     PrintLine(TEXT("Hi there! MOOO"));
     PrintLine(TEXT("Time to play"));
+
+    SetupGame();
+
+    PrintLine(TEXT("Guess the %i letter word..."), HiddenWord.Len());
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("Arduino");
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You win"));
+    }
+    else
+    {
+        if (Input.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("The input word has an incorrect lenght\n the hidden word has %i letters"), HiddenWord.Len());
+        }
+
+        PrintLine(TEXT("Try again"));
+    }
+
+    //if Input is valid then
+    //  check word
+    //else
+    //  print fail msg
+    //  remove life
+    //if lives > 0
+    //  try again loop
+    //else
+    //  lose msg
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("cake");
+    Lives = 4;
 }
